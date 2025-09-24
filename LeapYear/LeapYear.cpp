1af64886adfa8a2d0337bfc3e76c4d63031c93ef
Oleg Mamrykin <movesoft@yandex.ru>
2025-09-24 11:32:24 +0400

    fix all bugs

--- LeapYear/LeapYear.cpp
+++ LeapYear/LeapYear.cpp
@@ -7,5 +7,9 @@
 // Разместите здесь код функции, определяющей, является ли указанный год високосным
 bool IsLeapYear(int year)
 {
+	if (year < 0) return false;
+	if (year % 400 == 0) return true;
+	if (year % 100 == 0) return false;
+	if (year % 4 == 0) return true;
 	return false;
 }
